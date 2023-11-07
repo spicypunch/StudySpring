3d57095f079ac13a33e5b5d6a227b15d2899a83c
김종민 <jm.kim@lifesemantics.kr>
2023-11-07 11:20:35 +0900

    Controller 수정

--- src/main/kotlin/com/example/study/blog/controller/BlogController.kt
+++ src/main/kotlin/com/example/study/blog/controller/BlogController.kt
@@ -4,20 +4,23 @@ import com.example.study.blog.dto.BlogDto
 import com.example.study.blog.service.BlogService
 import com.example.study.entitiy.Wordcount
 import jakarta.validation.Valid
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RequestMapping("/api/blog")
 @RestController
 class BlogController(
         val blogService: BlogService
 ) {
+//        @GetMapping("")
+//        fun search(@RequestBody @Valid blogDto: BlogDto): String? {
+//            return blogService.searchKakao(blogDto)
+//        }
     @GetMapping("")
-    fun search(@RequestBody @Valid blogDto: BlogDto): String? {
+    fun search(@RequestParam query: String, @RequestParam sort: String, @RequestParam page: Int, @RequestParam size: Int): String? {
+        val blogDto = BlogDto(query, sort, page, size)
         return blogService.searchKakao(blogDto)
     }
+
 
     @GetMapping("/rank")
     fun searchWordRank(): List<Wordcount> = blogService.searchWordRank()
